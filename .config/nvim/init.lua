ad798d1829d963d941c973f51f7b86ce61ce456b
bjluckow <bjluckow1@gmail.com>
2026-01-21 17:45:31 -0500

    update nvim: telescope+tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,8 +13,19 @@ require("lazy").setup({
     vim.lsp.enable("pyright")
     end
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+    require("nvim-tree").setup()
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 })
-
 
 
 vim.opt.number = true
@@ -34,3 +45,12 @@ vim.api.nvim_create_autocmd("CursorHold", {
     end
   end,
 })
+
+-- telescope
+require("telescope").setup({})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
